52ff8005cd6923b5cfc1c9577b0f7f17d726256a
Charalampos Mainas <charalampos.mainas@gmail.com>
2021-08-27 11:09:19 +0200

    Scheduler: Switch from signals to eventfd and epoll
    Signal communication between the main thread (scheduler) and the thread
    which receives the new task has been replaced from a epoll amd eventfd
    scheme.
    
    There will be only one thread, the scheduler and it will uses other
    threads as workers in order to communicate with the outter world.
    Every new connection in the frontend socket (it will be used for new
    binaries) will be handled by a new thread, which is responsible to
    create the new task struct and notify the scheduler for the new task.
    
    The communication between the worker threads and the scheduler will be
    done through eventfds.
    
    Signed-off-by: Charalampos Mainas <charalampos.mainas@gmail.com>

--- scheduler/primary.c
+++ scheduler/primary.c
@@ -7,12 +7,12 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdint.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <sys/epoll.h>
+#include <sys/eventfd.h>
 
 #define FRONT_SOCK "/tmp/front.sock"
 #define BIN_PATH_LEN 24
+#define MAX_EVENTS 10
 
 enum task_state {
 	ready = 0,
@@ -41,9 +41,10 @@ struct node {
 	enum node_state state;
 };
 
-uint32_t nr_tsks;
-pthread_mutex_t tasks_lock;
-struct task *tsk_to_add;
+struct thr_data {
+	int socket;
+	int efd;
+};
 
 static struct task *create_new_task(char *path)
 {
@@ -55,7 +56,6 @@ static struct task *create_new_task(char *path)
 		return NULL;
 	}
 
-	new_task->id = nr_tsks++;
 	new_task->bin_path = strdup(path);
 	new_task->node_id = 0;
 	new_task->state = ready;
@@ -64,11 +64,50 @@ static struct task *create_new_task(char *path)
 	return new_task;
 }
 
-static void *frontend_link(void *arg)
+static void *get_cmd_front(void *arg)
 {
+	int con, rc;
+	char bin_path[BIN_PATH_LEN];
+	struct thr_data *td = (struct thr_data *) arg;
+	struct task *tsk_to_add = NULL;
+
+	uint64_t mpla = 17;
+
+	con = accept(td->socket, NULL, NULL);
+	if (con == -1) {
+		perror("frontend socket accept");
+	}
+
+	rc = read(con, bin_path, BIN_PATH_LEN);
+	if (rc <= 0) {
+		fprintf(stderr, "Read from peer failed");
+		close(con);
+	}
+	/* Make sure we do not read rubbish, old commands etc */
+	bin_path[(rc > BIN_PATH_LEN) ? BIN_PATH_LEN - 1: rc] = '\0';
+
+	printf("New binary at %s\n", bin_path);
+	tsk_to_add = create_new_task(bin_path);
+	if (!tsk_to_add)
+		fprintf(stderr, "Could not create new task\n");
+
+	rc = write(td->efd, &tsk_to_add, sizeof(uint64_t));
+	if (rc < 0)
+		perror("write new task in eventfd");
+
+	close(con);
+}
+
+int main()
+{
+	uint32_t nr_tsks = 0, nr_efds = 0;
+	struct task *tsk_to_add = NULL;
+	struct task *tsk_head = NULL, *tsk_last = NULL;
+	struct epoll_event ev, events[MAX_EVENTS];
+	int rc, epoll_ret, epollfd, front_sock, efds[MAX_EVENTS - 1];
 	struct sockaddr_un front_sockaddr;
-	int rc, front_sock;
-	pthread_t sched_thr = *(pthread_t *)arg;
+	struct task *tmp;
+	uint64_t eeval;
 
 	front_sock = socket(AF_UNIX, SOCK_STREAM, 0);
 	if (front_sock == -1) {
@@ -82,108 +121,80 @@ static void *frontend_link(void *arg)
 		  sizeof(struct sockaddr_un));
 	if (rc == -1) {
 		perror("socket bind");
-		rc = errno;
-		close(front_sock);
-		fprintf(stderr, "Socket bind error: %d\n", rc);
+		goto out_soc;
 	}
 
-	rc = listen(front_sock, 5);
+	rc = listen(front_sock, MAX_EVENTS - 1);
 	if (rc == -1) {
-		rc = errno;
-		close(front_sock);
-		fprintf(stderr, "Socket listen error: %d\n", rc);
+		perror("Frontend socket listen");
+		goto out_soc;
 	}
 
-	printf("listesning commands at %s\n", FRONT_SOCK);
-	for (;;) {
-		int con;
-		char bin_path[BIN_PATH_LEN];
+	epollfd = epoll_create1(0);
+	if (epollfd == -1) {
+		perror("epoll_create1");
+		goto out_soc;
+	}
 
-		con = accept(front_sock, NULL, NULL);
-		if (con == -1) {
-			rc = errno;
-			close(front_sock);
-			fprintf(stderr, "Socket accept error: %d\n", rc);
+	ev.events = EPOLLIN | EPOLLET;
+	ev.data.fd = front_sock;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, front_sock, &ev) == -1) {
+		perror("epoll_ctl: frontend socket");
+		goto out_pol;
+	}
+
+	printf("Listesning commands at %s\n", FRONT_SOCK);
+
+	while(1) {
+		int i;
+		pthread_t worker;
+
+		epoll_ret = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+		if (epoll_ret == -1) {
+			perror("epoll_wait");
+			goto out_pol;
 		}
 
-		rc = read(con, bin_path, BIN_PATH_LEN);
-		if (rc <= 0) {
-			close(con);
-			fprintf(stderr, "Read from peer failed");
+		for (i = 0; i < epoll_ret; i++) {
+			if (events[i].events == 0)
+				continue;
+			if (events[i].data.fd == front_sock) {
+				struct thr_data worker_data;
+
+				rc = eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK);
+				if (rc < 0) {
+					perror("Eventfd for frontend socket");
+					goto out_pol;
+				}
+
+				worker_data.socket = front_sock;
+				worker_data.efd = rc;
+				ev.events = EPOLLIN | EPOLLET;
+				ev.data.fd = rc;
+				if (epoll_ctl(epollfd, EPOLL_CTL_ADD, rc, &ev) == -1) {
+					perror("epoll_ctl: eventfd");
+					goto out_pol;
+				}
+
+				rc = pthread_create(&worker, NULL, get_cmd_front,
+						   (void *) &worker_data);
+				if (rc) {
+					perror("Frontend worker create");
+					goto out_pol;
+				}
+			} else {
+				rc = read(events[i].data.fd, &eeval, sizeof(uint64_t));
+				if (rc < 0) {
+					perror("Rread eventfd");
+					goto out_pol;
+				}
+				tsk_to_add = (struct task *) eeval;
+				tsk_to_add->id = nr_tsks++;
+				if (close(events[i].data.fd) == -1)
+					perror("Close eventfd");
+			}
+
 		}
-		/* Make sure we do not read rubbish, old commands etc */
-		/*
-		 * Edw bazei ena new line...
-		 */
-		printf("bytes = %d\n", rc);
-		bin_path[(rc > BIN_PATH_LEN) ? BIN_PATH_LEN - 1: rc] = '\0';
-
-		printf("New binary at %s\n", bin_path);
-		tsk_to_add = create_new_task(bin_path);
-		if (!tsk_to_add)
-			fprintf(stderr, "Could not create new task\n");
-
-		rc = pthread_kill(sched_thr, SIGUSR1);
-		if (rc < 0)
-			perror("signal to scheduler thread\n");
-		close(con);
-	}
-}
-
-int main()
-{
-	pthread_t frontend_thr, sched_thr = pthread_self();
-	sigset_t waitset;
-	struct sigaction siga;
-	struct task *tsk_head = NULL, *tsk_last = NULL;
-	int rc, sig;
-
-	struct task *tmp;
-
-	rc  = pthread_mutex_init(&tasks_lock, NULL);
-	if (rc !=0 ) {
-		fprintf(stderr, "Could not init task mutex: %d\n", rc);
-		exit(1);
-	}
-
-	nr_tsks = 0;
-
-	siga.sa_handler = SIG_DFL;
-	if(sigaction(SIGUSR1, &siga, NULL) == -1) {
-		perror("Deleting SIGUSR1 handler");
-		exit(1);
-	}
-
-	sigemptyset(&waitset);
-	if(sigaddset(&waitset, SIGUSR1) == -1) {
-		perror("Sigaddset scheduler thread");
-		exit(1);
-	}
-	rc = pthread_sigmask(SIG_BLOCK, &waitset, NULL);
-	if (rc) {
-		fprintf(stderr, "Could not block SIG_DFL %d\n", rc);
-		exit(1);
-	}
-
-	rc = pthread_create(&frontend_thr, NULL, frontend_link, (void *) &sched_thr);
-	if (rc) {
-		fprintf(stderr, "Could not create fontend thread %d\n", rc);
-		exit(1);
-	}
-
-	while(1)
-	{
-		printf("helo\n");
-		rc = sigwait(&waitset, &sig);
-		if (rc == -1 && !(errno == EAGAIN || errno == EINTR)) {
-			perror("Sigaddset scheduler thread");
-			exit(1);
-		}
-		printf("helo 1\n");
-
-		/*
-		 * Got new binary?
-		 */
 		if (!tsk_to_add)
 			continue;
 
@@ -193,12 +204,20 @@ int main()
 			tsk_last->next = tsk_to_add;
 			tsk_last = tsk_to_add;
 		}
+		tsk_to_add = NULL;
 
 		tmp = tsk_head;
 		while(tmp) {
 			printf("Task with id %d and bin at %s\n", tmp->id, tmp->bin_path);
-			printf("%s -- %s\n", tmp->bin_path, tmp->bin_path);
 			tmp = tmp->next;
 		}
 	}
+
+	return 0;
+
+out_pol:
+	close(epollfd);
+out_soc:
+	close(front_sock);
+	exit(EXIT_FAILURE);
 }
