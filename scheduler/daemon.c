21bdff8cf9251bd4e675868215e5004d88fa8ccd
Charalampos Mainas <charalampos.mainas@gmail.com>
2021-09-03 15:54:05 +0200

    Scheduler: Execute ukvm command when receiving guest binary
    Daemon will run the guest binary that got received from primary
    scheduler. The output is stored in the file "/tmp/guest_output".
    
    Moreover the daemon will store one and only binary each time and it will
    overwrite previous ones if it receives a new binary. The file where they
    will stored is "/tmp/binary.ukvm"
    
    Signed-off-by: Charalampos Mainas <charalampos.mainas@gmail.com>

--- scheduler/daemon.c
+++ scheduler/daemon.c
@@ -9,19 +9,22 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/epoll.h>
 
 #define BIN_PATH_LEN	254
+#define UKVM_BIN	"/home/cmainas/workspace/fpga_uni/funky-solo5/ukvm/ukvm-bin"
 
-static int write_file_n(uint8_t *buf, off_t size)
+static uint64_t write_file_n(uint8_t *buf, off_t size)
 {
 	int fd;
 	off_t offset = 0;
 	char file[BIN_PATH_LEN];
 	size_t n;
-	static int ref = 0;
+	static uint64_t ref = 0;
 
-	sprintf(file, "/tmp/binary_%d.ukvm", ref);
-	fd = open(file, O_WRONLY | O_CREAT, 0644);
+	//sprintf(file, "/tmp/binary.ukvm", ref);
+	sprintf(file, "/tmp/binary.ukvm");
+	fd = open(file, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd < 0) {
 		perror("Creating new file");
 		return -1;
@@ -38,7 +41,7 @@ static int write_file_n(uint8_t *buf, off_t size)
 		offset += n;
 	}
 
-	return 0;
+	return ref;
 }
 
 static uint8_t *read_file_n(int soc, off_t size)
@@ -71,13 +74,94 @@ err_read_f:
 	return NULL;
 }
 
-int main(int argc, char *argv[])
+static int handle_epoll(int epollfd, int socket, int sigfd)
 {
-	int rc, sched_sock, port = 0;
-	struct sockaddr_in sched_sockaddr;
-	char *ip_addr = NULL;
+	int rc;
+	int epoll_ret, i;
+	struct epoll_event events[2];
 	off_t bin_size;
 	uint8_t *buf;
+
+	epoll_ret = epoll_wait(epollfd, events, 2, -1);
+	if (epoll_ret == -1) {
+		perror("epoll wait");
+		return -1;;
+	}
+
+	for (i = 0; i < epoll_ret; i++) {
+		if (events[i].data.fd == socket) {
+			
+			rc = read(socket, &bin_size, sizeof(off_t));
+			if (rc == 0) {
+				perror("Read binary size\n");
+				return -1;
+			} else if (rc == 0) {
+				fprintf(stderr, "Loast connection with primary scheduler\n");
+				return -1;
+			} else if (rc < sizeof(off_t)) {
+				fprintf(stderr, "Short read on binary size\n");
+				return -1;
+			}
+			printf("size of binary is %ld\n", bin_size);
+
+			buf = read_file_n(socket, bin_size);
+			if (!buf)
+				return -1;
+
+			rc = write_file_n(buf, bin_size);
+			if (rc < 0)
+				return -1;
+			return 1;
+		} else if (events[i].data.fd == sigfd) {
+			printf("My child died?\n");
+
+		}
+	}
+	return 0;
+}
+
+static int start_guest()
+{
+	int pid;
+
+	pid  = fork();
+	if (pid == 0) {
+		/*
+		 * child
+		 */
+		int fd;
+		char *const  e_args[] = {UKVM_BIN, "--net=tap0", "--disk=/tmp/binary.ukvm", "/tmp/binary.ukvm", NULL};
+
+		fd = open("/tmp/guest_output", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+		if (fd < 0) {
+			perror("Opening redirected file\n");
+			exit(1);
+		}
+		if (dup2(fd, 1) < 0) {
+			perror("Redirecting stdout\n");
+			close(fd);
+			exit(1);
+		}
+		if (dup2(fd, 2) < 0) {
+			perror("Redirecting stderr\n");
+			close(fd);
+			exit(1);
+		}
+		execv(UKVM_BIN, e_args);
+	} else if (pid == -1) {
+		perror("fork");
+		return -1;
+	}
+
+	return pid;
+}
+
+int main(int argc, char *argv[])
+{
+	int rc, sched_sock, port = 0, epollfd;
+	struct sockaddr_in sched_sockaddr;
+	char *ip_addr = NULL;
+	struct epoll_event ev;
 
 	rc = getopt(argc, argv, "hi:p:");
 	while (rc != -1) {
@@ -121,28 +205,36 @@ int main(int argc, char *argv[])
 		goto out_soc;
 	}
 
-	rc = read(sched_sock, &bin_size, sizeof(off_t));
-	if (rc == 0) {
-		perror("Read binary size\n");
-		goto out_soc;
-	} else if (rc == 0) {
-		fprintf(stderr, "Loast connection with primary scheduler\n");
-		goto out_soc;
-	} else if (rc < sizeof(off_t)) {
-		fprintf(stderr, "Short read on binary size\n");
+	epollfd = epoll_create1(0);
+	if (epollfd == -1) {
+		perror("epoll_create1");
 		goto out_soc;
 	}
-	printf("size of binary is %ld\n", bin_size);
 
-	buf = read_file_n(sched_sock, bin_size);
-	if (!buf)
-		goto out_soc;
+	ev.events = EPOLLIN;
+	ev.data.fd = sched_sock;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sched_sock, &ev) == -1) {
+		perror("epoll_ctl: socket for scheduler");
+		goto out_pol;
+	}
 
-	rc = write_file_n(buf, bin_size);
-	if (rc < 0)
-		goto out_soc;
+	while(1)
+	{
+		rc = handle_epoll(epollfd, sched_sock, 0);
+		if (rc < 0)
+			goto out_pol;
+		if (rc == 1) {
+			if (start_guest() < 0)
+				goto out_pol;
+			printf("Started ukvm with guest\n");
+		}
+
+	}
+
 	return 0;
 
+out_pol:
+	close(epollfd);
 out_soc:
 	close(sched_sock);
 	exit(EXIT_FAILURE);
