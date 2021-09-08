ef394c91580f4e213ded99b3bf47c28aa56988c3
Charalampos Mainas <charalampos.mainas@gmail.com>
2021-09-08 16:03:47 +0200

    Scheduler: Migrate task to a different node
    When a node receives a migration command it will set up a connection
    with the new node where the task will run. The initial node will send
    both the binary and the migration file directly to the new node.
    
    After receiving theses 2 files the new node will start ukvm using
    "--load" option and the task will continue its execution in the new node.
    
    Signed-off-by: Charalampos Mainas <charalampos.mainas@gmail.com>

--- scheduler/daemon.c
+++ scheduler/daemon.c
@@ -12,12 +12,14 @@
 #include <sys/epoll.h>
 #include <sys/signalfd.h>
 #include <sys/un.h>
+#include <sys/sendfile.h>
 
 #include <signal.h>
 
 #define BIN_PATH_LEN	254
 #define UKVM_BIN	"/home/cmainas/workspace/fpga_uni/funky-solo5/ukvm/ukvm-bin"
 #define UKVM_SOC	"/tmp/ukvm_socket"
+#define PORT_NODES	1742
 
 struct com_nod {
 	uint8_t type;
@@ -27,16 +29,15 @@ struct com_nod {
 	};
 };
 
-static uint64_t write_file_n(uint8_t *buf, off_t size)
+static struct in_addr to_node;
+
+static uint64_t write_file_n(uint8_t *buf, off_t size, char *file)
 {
 	int fd;
 	off_t offset = 0;
-	char file[BIN_PATH_LEN];
 	size_t n;
 	static uint64_t ref = 0;
 
-	//sprintf(file, "/tmp/binary.ukvm", ref);
-	sprintf(file, "/tmp/binary.ukvm");
 	fd = open(file, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd < 0) {
 		perror("Creating new file");
@@ -124,7 +125,112 @@ mig_fail:
 	return -1;
 }
 
-static int handle_epoll(int epollfd, int socket, int sigfd)
+static ssize_t receive_a_file(int socket, char *file)
+{
+	ssize_t rc;
+	off_t file_sz;
+	uint8_t *buf;
+
+	rc = read(socket, &file_sz, sizeof(off_t));
+	if (rc < 0) {
+		perror("Read file\n");
+		return -1;
+	} else if (rc == 0) {
+		fprintf(stderr, "Lost connection with sender\n");
+		return -1;
+	} else if (rc < sizeof(off_t)) {
+		fprintf(stderr, "Short read on file size\n");
+		return -1;
+	}
+	printf("size of binary is %ld\n", file_sz);
+
+	buf = read_file_n(socket, file_sz);
+	if (!buf)
+		return -1;
+
+	rc = write_file_n(buf, file_sz, file);
+	free(buf);
+	if (rc < 0)
+		return -1;
+
+	return 0;
+}
+
+static ssize_t send_binary(int socket, char *file)
+{
+	int fd;
+	struct stat st;
+	ssize_t n, count = 0;
+	ssize_t rc = 0;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0) {
+		perror("Opening file to send");
+		return -1;
+	}
+
+	rc = stat(file, &st);
+	if (rc < 0) {
+		perror("binary size");
+		goto err_send;
+	}
+
+	rc = write(socket, &st.st_size, sizeof(off_t));
+	if (rc < sizeof(off_t)) {
+		if (rc < 0)
+			perror("Send file size");
+		else
+			fprintf(stderr, "Short send of binary size\n");
+		goto err_send;
+	}
+
+	while (count < st.st_size) {
+		n = sendfile(socket, fd, NULL, st.st_size - count);
+		if (n < 0) {
+			perror("Sending file");
+			goto err_send;
+		}
+		count += n;
+	}
+	return rc;
+
+err_send:
+	close(fd);
+	return -1;
+}
+
+static void send_migration()
+{
+	int sockfd, rc;
+	struct sockaddr_in saddr;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		fprintf(stderr, "socket error %d\n", errno);
+		return;
+	}
+
+	memcpy(&saddr.sin_addr, &to_node, sizeof(struct in_addr));
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(PORT_NODES);
+	rc = connect(sockfd, (struct sockaddr *) &saddr,
+		  sizeof(struct sockaddr_in));
+	if (rc == -1) {
+		perror("socket connect");
+		close(sockfd);
+		return;
+	}
+
+	rc = send_binary(sockfd, "/tmp/binary.ukvm");
+	if (rc < 0)
+		return;
+
+	rc = send_binary(sockfd, "/tmp/file.mig");
+	if (rc < 0)
+		return;
+}
+
+static int handle_epoll(int epollfd, int socket, int sigfd, int server_soc)
 {
 	int rc;
 	int epoll_ret, i;
@@ -159,7 +265,7 @@ static int handle_epoll(int epollfd, int socket, int sigfd)
 				if (!buf)
 					return -1;
 
-				rc = write_file_n(buf, node_com.size);
+				rc = write_file_n(buf, node_com.size, "/tmp/binary.ukvm");
 				free(buf);
 				if (rc < 0)
 					return -1;
@@ -170,6 +276,7 @@ static int handle_epoll(int epollfd, int socket, int sigfd)
 
 				printf("I will have to migrate to %s\n", myIP);
 				rc =start_migration();
+				to_node = node_com.rcv_ip;
 				if (rc < 0)
 					return -1;
 				return 2;
@@ -188,20 +295,40 @@ static int handle_epoll(int epollfd, int socket, int sigfd)
 			}
 			if (sinfo.ssi_signo == SIGCHLD) {
 				printf("My child died? with code %d\n", sinfo.ssi_status);
-				if (sinfo.ssi_status == 0)
+				if (sinfo.ssi_status == 0) {
 					return 4;
-				else if (sinfo.ssi_status == 7)
-					return 6;
-				else
+				} else if (sinfo.ssi_status == 7) {
+					send_migration();
+					return 7;
+				} else {
 					return 5;
+				}
 			}
 
+		} else if (events[i].data.fd == server_soc) {
+			int mig_soc;
+
+			mig_soc = accept(server_soc, NULL, NULL);
+			if (mig_soc == -1) {
+				perror("frontend socket accept");
+				return -1;
+			}
+			printf("Connection established\n");
+
+			rc = receive_a_file(mig_soc, "/tmp/rcvd_file.ukvm");
+			if (rc < 0)
+				return -1;
+
+			rc = receive_a_file(mig_soc, "/tmp/rcvd_file.mig");
+			if (rc < 0)
+				return -1;
+			return 3;
 		}
 	}
 	return 0;
 }
 
-static int start_guest()
+static int start_guest(uint8_t mig)
 {
 	int pid;
 
@@ -211,7 +338,6 @@ static int start_guest()
 		 * child
 		 */
 		int fd;
-		char *const  e_args[] = {UKVM_BIN, "--net=tap0", "--disk=/tmp/binary.ukvm", "--mon="UKVM_SOC, "/tmp/binary.ukvm", NULL};
 
 		fd = open("/tmp/guest_output", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 		if (fd < 0) {
@@ -228,7 +354,13 @@ static int start_guest()
 			close(fd);
 			exit(1);
 		}
-		execv(UKVM_BIN, e_args);
+		if (!mig) {
+			char *const  e_args[] = {UKVM_BIN, "--net=tap0", "--disk=/tmp/binary.ukvm", "--mon="UKVM_SOC, "/tmp/binary.ukvm", NULL};
+			execv(UKVM_BIN, e_args);
+		} else {
+			char *const  e_args[] = {UKVM_BIN, "--net=tap0", "--disk=/tmp/binary.ukvm", "--mon="UKVM_SOC, "--load=/tmp/file.mig", "/tmp/binary.ukvm", NULL};
+			execv(UKVM_BIN, e_args);
+		}
 	} else if (pid == -1) {
 		perror("fork");
 		return -1;
@@ -252,9 +384,66 @@ static int send_deploy_res(int socket, int res)
 	return 0;
 }
 
+static int setup_socket(int domain, int epollfd)
+{
+	int sock, rc;
+	struct epoll_event ev;
+
+	sock = socket(domain, SOCK_STREAM, 0);
+	if (sock == -1) {
+		fprintf(stderr, "socket error %d\n", errno);
+		return -1;;
+	}
+
+	if (domain == AF_UNIX) {
+		struct sockaddr_un saddr_un = {0};
+
+		saddr_un.sun_family = AF_UNIX;
+		strcpy(saddr_un.sun_path, UKVM_SOC);
+		unlink(UKVM_SOC);
+		rc = bind(sock, (struct sockaddr *) &saddr_un,
+				sizeof(struct sockaddr_un));
+	} else if (domain == AF_INET) {
+		struct sockaddr_in saddr_in = {0};
+
+		saddr_in.sin_family = AF_INET;
+		saddr_in.sin_addr.s_addr = htonl(INADDR_ANY);
+		saddr_in.sin_port = htons(PORT_NODES);
+		rc = bind(sock, (struct sockaddr *) &saddr_in,
+				sizeof(struct sockaddr_in));
+	} else {
+		fprintf(stderr, "Invalid domain\n");
+		goto err_set;
+	}
+
+	if (rc == -1) {
+		perror("socket bind");
+		goto err_set;
+	}
+
+	rc = listen(sock, 2);
+	if (rc == -1) {
+		perror("Socket listen");
+		goto err_set;
+	}
+
+	ev.events = EPOLLIN | EPOLLET;
+	ev.data.fd = sock;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sock, &ev) == -1) {
+		perror("epoll_ctl: frontend socket");
+		goto err_set;
+	}
+
+	return sock;
+
+err_set:
+	close(sock);
+	return -1;
+}
+
 int main(int argc, char *argv[])
 {
-	int rc, sched_sock, port = 0, epollfd, sfd;
+	int rc, sched_sock, port = 0, epollfd, sfd, server_soc;
 	struct sockaddr_in sched_sockaddr;
 	char *ip_addr = NULL;
 	struct epoll_event ev;
@@ -315,43 +504,57 @@ int main(int argc, char *argv[])
 		goto out_pol;
 	}
 
+	server_soc = setup_socket(AF_INET, epollfd);
+	if (server_soc < 0) {
+		fprintf(stderr, "Could not setup_socket for inter node communication\n");
+		goto out_pol;
+	}
+
 	sigemptyset(&schld_set);
 	sigaddset(&schld_set, SIGCHLD);
 	if (sigprocmask(SIG_BLOCK, &schld_set, NULL) == -1) {
 		perror("Change handling of SIGCHLD");
-		goto out_pol;
+		goto out_socs;
 	}
 
 	sfd = signalfd(-1, &schld_set, 0);
 	if (sfd < 0) {
 		perror("Create signalfd");
-		goto out_pol;
+		goto out_socs;
 	}
 	ev.events = EPOLLIN;
 	ev.data.fd = sfd;
 	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sfd, &ev) == -1) {
 		perror("epoll_ctl: socket for scheduler");
-		goto out_pol;
+		goto out_socs;
 	}
 
 	while(1)
 	{
-		rc = handle_epoll(epollfd, sched_sock, sfd);
-		if (rc < 0)
-			goto out_pol;
-		else if (rc == 1) {
-			if (start_guest() < 0)
-				goto out_pol;
+		rc = handle_epoll(epollfd, sched_sock, sfd, server_soc);
+		if (rc < 0) {
+			goto out_socs;
+		} else if (rc == 1) {
+			if (start_guest(0) < 0)
+				goto out_socs;
 			printf("Started ukvm with guest\n");
-		} else if (rc ==2) {
+		} else if (rc == 3) {
+			if (start_guest(1) < 0)
+				goto out_socs;
+			printf("Started ukvm with migrated guest\n");
+		} else if (rc == 2) {
+			continue;
+		} else {
 			if (send_deploy_res(sched_sock, rc) < 0)
-				goto out_pol;
+				goto out_socs;
 		}
 
 	}
 
 	return 0;
 
+out_socs:
+	close(server_soc);
 out_pol:
 	close(epollfd);
 out_soc:
